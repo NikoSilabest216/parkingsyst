f2c28c99b1e9aa37e08f14004c3de2d334fc76ab
NikoSilabest216 <silabestniko@gmail.com>
2022-03-14 09:24:40 +0700

    first commit

---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,21 @@
+<header class="main-header">
+
+
+    <!-- Logo -->
+    <a href="" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><img src="..\assets\dist\img\rok.png" width="50px", height="50px" alt="Maklo"></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><img src="..\assets\dist\img\umm.png" width="200px", height="60px" alt="Maklo"></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  
