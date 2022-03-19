78217562d332eee274ed4a863aac796788ae2454
NikoSilabest216 <silabestniko@gmail.com>
2022-03-20 06:08:17 +0700

    Baru

--- application/views/templates/header_menu.php
+++ application/views/templates/header_menu.php
@@ -2,11 +2,11 @@
 
 
     <!-- Logo -->
-    <a href="" class="logo">
+    <a href="https://www.gitlab.com/" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><img src="..\assets\dist\img\rok.png" width="50px", height="50px" alt="Maklo"></span>
+      <span class="logo-mini"><img src="https://gitlab.com/NikoSilabest216/discrot/-/raw/main/rok.png" width="50px", height="50px" alt="Maklo"></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><img src="..\assets\dist\img\umm.png" width="200px", height="60px" alt="Maklo"></span>
+      <span class="logo-lg"><img src="https://gitlab.com/NikoSilabest216/discrot/-/raw/main/umm.png" width="200px", height="60px" alt="Maklo"></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
